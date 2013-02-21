6865f6583cca3ccd58de2abf678586d6291c1519
Laura Brown <laurabrown1113@gmail.com>
2013-02-20 19:41:13 -0500

    2013-02-20 19:41

--- plugins/vimeo.rb
+++ plugins/vimeo.rb
@@ -7,9 +7,13 @@ module Jekyll
     end
 
     def render(context)
-      %(<div class="embed-video-container"><iframe src="http://player.vimeo.com/video/#{@id}"></iframe></div>)
+      %(<div class="embed-video-container"><iframe src="http://player.vimeo.com/video/#{@id}" width="100%" height="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>)
     end
   end
 end
 
 Liquid::Template.register_tag('vimeo', Jekyll::Vimeo)
+
+
+
+
